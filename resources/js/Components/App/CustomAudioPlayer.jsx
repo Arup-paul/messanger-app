a0def78758d076812a7b82605af9496b5c11dc56
arup-paul <aruppaulctg@gmail.com>
2024-06-26 01:13:55 +0600

    attachment preview desgin

--- resources/js/Components/App/CustomAudioPlayer.jsx
+++ resources/js/Components/App/CustomAudioPlayer.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const CustomAudioPlayer = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
+
+
+//////////////////  6.01
 
 export default CustomAudioPlayer;
